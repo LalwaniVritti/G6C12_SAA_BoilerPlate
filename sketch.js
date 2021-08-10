9d34c67692b1856069afa029591107bf5409a2e2
LalwaniVritti <88480121+LalwaniVritti@users.noreply.github.com>
2021-08-10 18:29:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var day;
+var month;
+
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+
+  textSize(20);
+
+  //add switch case for day and month name
+  
+}
